e78d52691520a4ffa10630d6392ef2673197f38a
Vladimir <rva2007@yandex.com>
2021-09-28 17:48:21 +0700

    добавлены файлы проекта

--- app/src/main/java/com/example/braintraining/MainActivity.java
+++ app/src/main/java/com/example/braintraining/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickFalse(View view) {
-        numbers();
+//        numbers();
     }
 
     private void numbers() {
