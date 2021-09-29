41536cbac27666e8ab84d2885c23bde5df24fa4d
Vladimir <rva2007@yandex.com>
2021-09-29 21:14:34 +0700

    исправлена генерация случайных чисел в MainActivity

--- app/src/main/java/com/example/braintraining/MainActivity.java
+++ app/src/main/java/com/example/braintraining/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tvMain, tvResult;
+    private TextView tvIndex;
     private ActionBar actionBar;
     private long start_time = 0;
     private long current_time = 0;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        tvIndex = findViewById(R.id.tvIndex);
         start_time = System.currentTimeMillis();
         tvMain = findViewById(R.id.tvMain);
         tvResult = findViewById(R.id.tvResult);
@@ -76,16 +78,16 @@ public class MainActivity extends AppCompatActivity {
     private void numbers() {
         int max = 20;
         int min = 2;
-        int number_1 = (int) (Math.random() * (max - min));
-        int number_2 = (int) (Math.random() * (max - min));
-        int number_index = (int) (Math.random() * (3 - 1));
+        int number_1 = (int) (Math.random() * (max - min) + min);
+        int number_2 = (int) (Math.random() * (max - min) + min);
+        int number_index = (int) (Math.random() * (5 - 1) + 1);
         int max_false = 40;
         int min_false = 10;
-        int number_false = (int) (Math.random() * (max_false - min_false));
+        int number_false = (int) (Math.random() * (max_false - min_false) + min_false);
         int number_result = number_1 + number_2;
-
+tvIndex.setText(String.valueOf(number_index));
         String text;
-        if (number_index == 1) {
+        if (number_index != 3) {
             text = number_1 + " + " + number_2 + " = " + number_result;
             is_true_answer = true;
         } else {
@@ -95,8 +97,8 @@ public class MainActivity extends AppCompatActivity {
         tvMain.setText(text);
 
 
-        if (true_answer>= max_true_answer) {
-            Intent i = new Intent(this,FinalActivity.class);
+        if (true_answer >= max_true_answer) {
+            Intent i = new Intent(this, FinalActivity.class);
             i.putExtra("time_result", time_result);
             startActivity(i);
         }
